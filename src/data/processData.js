681b38671444d52c1c812481c7c0e44e40bd2559
Sandy <tottati@hotmail.com>
2019-12-04 14:07:00 +0100

    poi

--- src/data/processData.js
+++ src/data/processData.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 
-const data2012 = require('./population-tls-2012.json')
+const data2011 = require('./population-tls-2011.json')
 
-const dataPop2012 = data2012
+const dataPop2011 = data2011
 	.map(quartier => ({
 		libelleQuartier: quartier.fields.libgq,
-		population: quartier.fields.p12_pop,
-		pop0014: quartier.fields.p12_pop0014,
-		pop1529: quartier.fields.p12_pop1529,
-		pop3044: quartier.fields.p12_pop3044,
-		pop4559: quartier.fields.p12_pop4559,
-		pop6074: quartier.fields.p12_pop6074,
-		pop75p: quartier.fields.p12_pop75p,
+		population: quartier.fields.p11_pop,
+		pop0014: quartier.fields.p11_pop0014,
+		pop1529: quartier.fields.p11_pop1529,
+		pop3044: quartier.fields.p11_pop3044,
+		pop4559: quartier.fields.p11_pop4559,
+		pop6074: quartier.fields.p11_pop6074,
+		pop75p: quartier.fields.p11_pop75p,
 		coordinates: quartier.fields.geo_shape.coordinates,
 
 	}))
-const population2012 = JSON.stringify(dataPop2012, null, 2);
-fs.writeFileSync('population-toulouse-2012.json', population2012)
+const population2011 = JSON.stringify(dataPop2011, null, 2);
+fs.writeFileSync('population-toulouse-2011.json', population2011)
---
+++ src/data/processDataPoi.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+const poiJson = require('./points-dinteret.json')
+
+const poiData = poiJson
+	.filter(poi => /Sanisette|Marché|wifi|Patinoire|Skatepark|déjections/.test(poi.fields.categorie))
+	.map(poi => ({
+		nom: poi.fields.nom,
+		descriptif: poi.fields.descriptif,
+		adresse: poi.fields.adresse,
+		id: poi.fields.poi_id,
+		shape: poi.fields.geo_shape,
+		categorie: poi.fields.categorie
+
+	}))
+
+
+const poi = JSON.stringify(poiData, null, 2);
+fs.writeFileSync('poi-toulouse.json', poi)
